f313ff22848de3012251ee54f1cd53063e567634
Brianna Hickerson <76510136+aloofy92@users.noreply.github.com>
2023-07-04 00:41:29 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Box, Container, Typography, Button, Grid, Card, CardContent } from '@mui/material';
+import { Box, Container, Typography, Button } from '@mui/material';
 import { authCheck, logout } from '../redux/authSlice';
 import UserDashboard from './UserDashboards'; // Import the UserDashboard component
 
@@ -21,44 +21,26 @@ const Home = () => {
         <UserDashboard />
       ) : (
         <>
-          <Box mt={5}>
+          <Box mt={5} textAlign="center">
             <Typography variant="h1">Share Your Landlord Experiences</Typography>
             <Typography variant="h4" color="textSecondary">
               The platform you need to rate and review your landlords.
             </Typography>
           </Box>
 
-          <Box mt={5}>
-            <Grid container spacing={3}>
-              {featuredLandlords.map((landlord) => (
-                <Grid item xs={12} sm={6} md={4} key={landlord.id}>
-                  <Card>
-                    {/* Add landlord image here */}
-                    <CardContent>
-                      <Typography variant="h6" component="div">
-                        {landlord.name}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary">
-                        {landlord.description}
-                      </Typography>
-                      {/* Add additional landlord details */}
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
+          <Box mt={5} textAlign="center">
+            <Button variant="contained" color="primary" href="/login">
+              Login
+            </Button>
+            <Button variant="contained" color="secondary" href="/register">
+              Register
+            </Button>
           </Box>
 
           <Box mt={5} textAlign="center">
-            <>
-              <Button variant="contained" color="primary" href="/login">
-                Login
-              </Button>
-              <Button variant="contained" color="secondary" href="/register">
-                Register
-              </Button>
-            </>
+            <img src="images/manwithkeys.jpg" alt="Landlord" style={{ width: '50%', display: 'block', margin: '0 auto' }} />
           </Box>
+
         </>
       )}
     </Container>
@@ -66,3 +48,4 @@ const Home = () => {
 };
 
 export default Home;
+
