99b6bf2fc7e36fca88c4dc9ef8301f5c7f0b115a
Brianna Hickerson <76510136+aloofy92@users.noreply.github.com>
2023-08-31 13:43:09 -0700

    Update Axios.js

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const Axios = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL
+baseURL: process.env.REACT_APP_AXIOS === 'production' ? : '/api' :  ' http://localhost:3001/api'
 })
 
 export default Axios
